7e28b723a56ebc06dd55817de2f62d7f0f17ce97
Doan Thi Minh Hang <doanthiminhhangit@gmail.com>
2022-05-01 18:24:07 +0700

    make smoothier

--- Spaceship.cpp
+++ Spaceship.cpp
@@ -64,28 +64,74 @@ void spaceship::handleEvent(SDL_Event events, SDL_Renderer* renderer)
         switch(events.key.keysym.sym)
             {
                 case SDLK_ESCAPE: break;
+                case SDLK_a:
                 case SDLK_LEFT:
                     {
                         this->turnLeft();
                         this->move();
                         break;
                     }
+
+                case SDLK_d:
                 case SDLK_RIGHT:
                     {
                         this->turnRight();
                         this->move();
                         break;
                     }
+
+                case SDLK_s:
                 case SDLK_DOWN:
                     {
                         this->turnDown();
                         this->move();
                         break;
                     }
+
+                case SDLK_w:
                 case SDLK_UP:
                     {
                         this->turnUp();
                         this->move();
+                        break;
+                    }
+
+
+                case SDLK_v:
+                    {
+                        weaponOb* pBullet = new weaponOb();
+
+                        pBullet->setType(weaponOb::BLASTER);
+                        pBullet->loadTextureBullet(renderer, 1);
+
+                        SDL_Rect planeRect = this->getRect();
+                        int x_val = planeRect.x + planeRect.w/2 - 10;
+                        int y_val = planeRect.y - planeRect.h - 5;
+                        pBullet->setRect(x_val, y_val);
+
+                        pBullet->setIsMove(true);
+
+                        pWeaponList.push_back(pBullet);
+
+                        break;
+                    }
+
+                case SDLK_SPACE:
+                    {
+                         weaponOb* pBullet = new weaponOb();
+
+                         pBullet->setType(weaponOb::BORON);
+                         pBullet->loadTextureBullet(renderer, 1);
+
+                        SDL_Rect planeRect = this->getRect();
+                        int x_val = planeRect.x + planeRect.w/2 - 10;
+                        int y_val = planeRect.y - planeRect.h - 5;
+                        pBullet->setRect(x_val, y_val);
+
+                        pBullet->setIsMove(true);
+
+                        pWeaponList.push_back(pBullet);
+
                         break;
                     }
                 default: break;
@@ -103,8 +149,8 @@ void spaceship::handleEvent(SDL_Event events, SDL_Renderer* renderer)
             }
         }
     }
-
-    if(events.type == SDL_MOUSEBUTTONDOWN)
+    /*
+    if (events.type == SDL_MOUSEBUTTONDOWN)
     {
         weaponOb* pBullet = new weaponOb();
 
@@ -131,5 +177,5 @@ void spaceship::handleEvent(SDL_Event events, SDL_Renderer* renderer)
     else if(events.type == SDL_MOUSEBUTTONUP)
     {
 
-    }
+    }*/
 }
